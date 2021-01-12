0796c88caadd3c817ce95c1be4ef86e0e69b7c25
thampiotr <17101802+thampiotr@users.noreply.github.com>
2021-01-12 09:52:37 +0000

    download fzf bash scripts

--- download_tools.sh
+++ download_tools.sh
@@ -4,8 +4,8 @@
 
 OS="linux"
 ARCH="amd64"
-
-FZF_VERSION="0.25.0-${OS}_${ARCH}"
+FZF_VERSION_NUMBER="0.25.0"
+FZF_VERSION="${FZF_VERSION_NUMBER}-${OS}_${ARCH}"
 
 mkdir -p ./bin
 
@@ -14,4 +14,16 @@ then
     curl -L -o ./bin/fzf.tar.gz "https://github.com/junegunn/fzf/releases/download/0.25.0/fzf-${FZF_VERSION}.tar.gz" 
     tar xf "./bin/fzf.tar.gz" -C ./bin
     rm "./bin/fzf.tar.gz"
+fi
+
+mkdir -p ./shell
+
+if [ ! -f ./shell/completion.bash ];
+then
+    curl -L -o ./shell/completion.bash "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION_NUMBER}/shell/completion.bash"
+fi
+
+if [ ! -f ./shell/key-bindings.bash ];
+then
+    curl -L -o ./shell/key-bindings.bash "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION_NUMBER}/shell/key-bindings.bash"
 fi
